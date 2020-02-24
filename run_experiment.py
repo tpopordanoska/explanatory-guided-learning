2c7b2f1057e4ed666fd2392e80e3cb8b98ae2b19
Teodora Popordanoska <teodora.popordanoska@gmail.com>
2020-02-24 23:57:53 +0100

    Track execution time for each method and small changes for different experiments

--- run_experiment.py
+++ run_experiment.py
@@ -1,3 +1,5 @@
+import time
+
 from src import *
 from src.experiments import *
 
@@ -23,10 +25,10 @@ split_seed = 0
 model_seeds = [0]
 max_iter = 100
 n_folds = 10
-n_clusters = 10
+n_clusters_list = [10]
 plots_off = True
 # List of sampling strategies
-methods = ["al_least_confident", "sq_random"]
+methods = ["random", "al_least_confident", "sq_random"]
 thetas = [1.0, 0.1, 0.01]
 for theta in thetas:
     methods.append("egl_{}".format(theta))
@@ -51,17 +53,38 @@ for experiment_name in experiments:
     # List of models that will be run for each experiment
     experiment_path = create_experiment_folder(path_to_main_file, experiment_name)
     models = [
-        SVM(name="SVM rbf gamma=0.0001 C=10000", gamma=0.0001, C=10000),
-        SVM(name="SVM rbf gamma=0.01 C=100", gamma=0.01, C=100), # for breast-cancer and german and banknote
-        SVM(name='SVM rbf gamma=1 C=10', gamma=1, C=10), # for banknote (also others with gamma < 1)
-        SVM(name='SVM rbf gamma=10 C=10', gamma=10, C=10),  # for haberman's
+        # Synthetic
+        # SVM(name='SVM (gamma=100, C=100)', gamma=100, C=100),
+        # SVM(name='SVM (gamma=1000, C=10)', gamma=1000, C=10),
 
-        SVM(name='SVM rbf gamma=1000 C=1', gamma=1000, C=1), #for synthetic
-        SVM(name='SVM rbf gamma=1000 C=0.1', gamma=1000, C=0.1),
+        # Banknote
+        # SVM(name='SVM (gamma=1, C=10)', gamma=1, C=10),
+        # NeuralNetwork()
+        # GradientBoosting(),
 
-        RandomForrest(),
-        GradientBoosting(),
-        NeuralNetwork(name="MLP")
+        # Breast cancer
+        # SVM(name="SVM (gamma=0.01, C=100)", gamma=0.01, C=100),
+        # SVM(name="SVM"),
+        # RandomForrest(),
+        # NeuralNetwork(),
+
+        # Haberman's
+        # SVM(name='SVM (gamma=10, C=10)', gamma=10, C=10),
+        # SVM(name="SVM (gamma=0.01, C=100)", gamma=0.01, C=100),
+        # NeuralNetwork(),
+        # RandomForrest(),
+        # GradientBoosting(),
+
+        # German
+        # GradientBoosting(),
+        # NeuralNetwork(),
+        # RandomForrest()
+
+        # Adult
+        # GradientBoosting(),
+        # NeuralNetwork()
+        # RandomForrest(),
+        # SVM(name='SVM (gamma=1, C=10)', gamma=1, C=10)
     ]
     for model in models:
         scores_dict_f1 = {}
@@ -78,48 +101,57 @@ for experiment_name in experiments:
 
             experiment = EXPERIMENTS[experiment_name](model=model, tiny_clusters=tiny_clusters, balanced_db=balanced_db)
 
-            learning_loop = ActiveLearningLoop(experiment, n_clusters, max_iter, model_path, file, plots_off, thetas,
-                                               use_weights=use_weights, use_labels=use_labels)
+            for n_clusters in n_clusters_list:
+                learning_loop = ActiveLearningLoop(experiment, n_clusters, max_iter, model_path, file, plots_off, thetas,
+                                                   use_weights=use_weights, use_labels=use_labels)
 
-            # Split the data into labeled and unlabeled
-            folds = experiment.split(prop_known=experiment.prop_known, n_splits=n_folds, split_seed=split_seed)
-            for k, (known_idx, train_idx, test_idx) in enumerate(folds):
-                # Remove duplicates
-                known_idx, train_idx, test_idx = np.unique(known_idx), np.unique(train_idx), np.unique(test_idx)
-                file.write('split seed {}, model seed {}, fold {} : #known {}, #train {}, #test {} \n'
-                           .format(split_seed, seed, k + 1, len(known_idx), len(train_idx), len(test_idx)))
-                _, counts_known = np.unique(experiment.y[known_idx], return_counts=True)
-                _, counts_train = np.unique(experiment.y[train_idx], return_counts=True)
-                _, counts_test = np.unique(experiment.y[test_idx], return_counts=True)
-                file.write("Known bincount: {}\n".format(counts_known))
-                file.write("Train bincount: {}\n".format(counts_train))
-                file.write("Test bincount: {}\n".format(counts_test))
+                # Split the data into labeled and unlabeled
+                folds = experiment.split(prop_known=experiment.prop_known, n_splits=n_folds, split_seed=split_seed)
+                for k, (known_idx, train_idx, test_idx) in enumerate(folds):
+                    # Remove duplicates
+                    known_idx, train_idx, test_idx = np.unique(known_idx), np.unique(train_idx), np.unique(test_idx)
+                    file.write('split seed {}, model seed {}, fold {} : #known {}, #train {}, #test {} \n'
+                               .format(split_seed, seed, k + 1, len(known_idx), len(train_idx), len(test_idx)))
+                    _, counts_known = np.unique(experiment.y[known_idx], return_counts=True)
+                    _, counts_train = np.unique(experiment.y[train_idx], return_counts=True)
+                    _, counts_test = np.unique(experiment.y[test_idx], return_counts=True)
+                    file.write("Known bincount: {}\n".format(counts_known))
+                    file.write("Train bincount: {}\n".format(counts_train))
+                    file.write("Test bincount: {}\n".format(counts_test))
 
-                X_initial = Normalizer(experiment.normalizer).normalize(experiment.X)
-                if experiment.X.shape[1] > 2:
-                    X_initial = get_tsne_embedding(X_initial)
-                plot_points(X_initial, experiment.y, "Initial points", model_path)
+                    X_initial = Normalizer(experiment.normalizer).normalize(experiment.X)
+                    if experiment.X.shape[1] > 2:
+                        X_initial = get_tsne_embedding(X_initial)
+                    plot_points(X_initial, experiment.y, "Initial points", model_path)
+                    plot_points(X_initial[known_idx], experiment.y[known_idx], "Known points", model_path)
+                    plot_points(X_initial[test_idx], experiment.y[test_idx], "Test points", model_path)
 
-                for method in methods:
-                    print(method)
-                    file.write("Method: {} \n".format(method))
-                    file.write("Model: {}\n".format(experiment.model._model))
-                    file.write("Using {} clusters, {} folds, {} model seeds, {} thetas\n".format(n_clusters, n_folds, model_seeds, thetas))
-                    file.write("use_weights={}, use_labels={}, n_clusters={}\n".format(use_weights, use_labels, n_clusters))
-                    scores_f1, test_scores_f1, scores_auc, test_scores_auc = learning_loop.run(method, known_idx, train_idx, test_idx)
-
-                    if method not in scores_dict_f1:
-                        scores_dict_f1[method] = [scores_f1]
-                        scores_test_dict_f1[method] = [test_scores_f1]
-                        scores_dict_auc[method] = [scores_auc]
-                        scores_test_dict_auc[method] = [test_scores_auc]
-                    else:
-                        scores_dict_f1[method].append(scores_f1)
-                        scores_test_dict_f1[method].append(test_scores_f1)
-                        scores_dict_auc[method].append(scores_auc)
-                        scores_test_dict_auc[method].append(test_scores_auc)
+                    for method in methods:
+                        print(method)
+                        file.write("Method: {} \n".format(method))
+                        file.write("Model: {}\n".format(experiment.model._model))
+                        file.write("Using {} clusters, {} folds, {} model seeds, {} thetas\n".format(n_clusters, n_folds, model_seeds, thetas))
+                        file.write("use_weights={}, use_labels={}, n_clusters={}\n".format(use_weights, use_labels, n_clusters))
+                        start = time.time()
+                        scores_f1, test_scores_f1, scores_auc, test_scores_auc = learning_loop.run(method, known_idx, train_idx, test_idx)
+                        end = time.time()
+                        execution_time = end-start
+                        print(execution_time)
+                        file.write("Execution time: " + str(execution_time))
+                        # key = str(n_clusters)
+                        key = method
+                        if key not in scores_dict_f1:
+                            scores_dict_f1[key] = [scores_f1]
+                            scores_test_dict_f1[key] = [test_scores_f1]
+                            scores_dict_auc[key] = [scores_auc]
+                            scores_test_dict_auc[key] = [test_scores_auc]
+                        else:
+                            scores_dict_f1[key].append(scores_f1)
+                            scores_test_dict_f1[key].append(test_scores_f1)
+                            scores_dict_auc[key].append(scores_auc)
+                            scores_test_dict_auc[key].append(test_scores_auc)
 
         plot_results(scores_dict_f1, scores_test_dict_f1, learning_loop.annotated_point, n_folds, experiment,
-                     split_seed, scorers[0], file, model_path)
+                     split_seed, scorers[0], file, model_path, max_iter)
         plot_results(scores_dict_auc, scores_test_dict_auc, learning_loop.annotated_point, n_folds, experiment,
-                     split_seed, scorers[1], file, model_path)
+                     split_seed, scorers[0], file, model_path, max_iter)
